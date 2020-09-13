a856320195dc104d1bf6e29b6b4c4175ec92a05e
Steve Schwarz <steve@agilitynerd.com>
2020-09-13 17:42:56 -0500

    Jest test of button click event

--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -1,12 +1,12 @@
 import '@testing-library/jest-dom/extend-expect'
 
 import { Large, Primary, Secondary, Small } from '@/stories/Button.stories'
-import { render, screen } from '@testing-library/vue'
+import { fireEvent, render, screen } from '@testing-library/vue'
 
 import Button from '@/components/Button.vue'
 
-it('renders the button in the primary state', () => {
-  render(Button, { props: Primary.args })
+it('renders the button in the primary state', async () => {
+  const { emitted } = render(Button, { props: Primary.args })
 
   const btn = screen.getByRole('button')
   expect(btn).toHaveTextContent('Button')
@@ -14,9 +14,11 @@ it('renders the button in the primary state', () => {
     'storybook-button--primary',
     'storybook-button--medium'
   )
+  await fireEvent.click(btn)
+  expect(emitted()).toHaveProperty('onClick')
 })
 
-it('renders the button in the secondary state', () => {
+it('renders the button in the secondary state', async () => {
   render(Button, { props: Secondary.args })
 
   const btn = screen.getByRole('button')
@@ -25,7 +27,7 @@ it('renders the button in the secondary state', () => {
   expect(btn).not.toHaveClass('storybook-button--primary')
 })
 
-it('renders the button in the Large state', () => {
+it('renders the button in the Large state', async () => {
   render(Button, { props: Large.args })
 
   const btn = screen.getByRole('button')
@@ -34,7 +36,7 @@ it('renders the button in the Large state', () => {
   expect(btn).not.toHaveClass('storybook-button--primary')
 })
 
-it('renders the button in the Large state', () => {
+it('renders the button in the Large state', async () => {
   render(Button, { props: Small.args })
 
   const btn = screen.getByRole('button')
